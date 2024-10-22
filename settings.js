f2809968d42fa305c2caee38fdb56a740a56d21d
darkknight152 <darkknight152p@gmail.com>
2024-10-22 16:54:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
+global.ownernomer = "94755095939"
+global.ownername = "dark crazy boy"
+global.ytname = "YT: darkcrazy-y6q"
+global.socialm = "GitHub: darkknight152"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '94755095939'  //creator number
+global.ownername = 'dark crazy boy' //owner name
+global.botname = 'Bug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +94755095939'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VacBHjBGzzKX6VTvdq3R'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
